0880ef98b197bb3dc997298d1c5394a899c0dc39
aitmil <120422761+aitmil@users.noreply.github.com>
2024-03-25 23:55:03 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,19 +10,32 @@ export function galleryTemplate(arr) {
         comments,
         downloads,
         ...info
-      }) => `<li class="gallery-item">
-        <div class="lightbox">
-          <a class="gallery-link" href="${largeImageURL}">
-            <img class="gallery-image" src="${webformatURL}" alt="${tags}"
-          /></a>
-        </div>
-        <ul class="gallery-data-list">
-          <li class="gallery-data-item">Likes${likes}</li>
-          <li class="gallery-data-item">Views${views}</li>
-          <li class="gallery-data-item">Comments${comments}</li>
-          <li class="gallery-data-item">Downloads${downloads}</li>
-        </ul>
-      </li>`
+      }) => ` <li class="gallery-item">
+      <div class="lightbox">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}"
+        /></a>
+      </div>
+      <ul class="gallery-data-list">
+        <li class="gallery-data-item">
+          <h2>Likes</h2>
+          <p>${likes}</p>
+        </li>
+        <li class="gallery-data-item">
+          <h2>Views</h2>
+          <p>${views}</p>
+        </li>
+        <li class="gallery-data-item">
+          <h2>Comment</h2>
+          <p>${comments}</p>
+        </li>
+        <li class="gallery-data-item">
+          <h2>Downloads</h2>
+          <p>${downloads}</p>
+        </li>
+      </ul>
+    </li>
+`
     )
     .join('');
 }
