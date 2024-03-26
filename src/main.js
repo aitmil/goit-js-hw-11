c1bea86422b9db1bf7ac8e241317c940f0075628
aitmil <120422761+aitmil@users.noreply.github.com>
2024-03-26 15:27:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { getImage } from './js/pixabay-api';
 import { galleryTemplate } from './js/render-functions';
 
 const searchForm = document.querySelector('.search-form');
-const imageList = document.querySelector('.image-list');
+const gallery = document.querySelector('.gallery');
 
 let lightbox = new SimpleLightbox('.lightbox a', { captionsData: 'alt' });
 lightbox.on('show.simplelightbox', function () {});
@@ -15,13 +17,13 @@ searchForm.addEventListener('submit', onSubmit);
 function onSubmit(evt) {
   evt.preventDefault();
   const query = evt.target.elements.search.value.trim();
-  imageList.innerHTML = '<div class="loader"></div>';
+  gallery.innerHTML = '<div class="loader"></div>';
 
   if (query !== '') {
     getImage(query)
       .then(data => {
         const markup = galleryTemplate(data.hits);
-        imageList.innerHTML = markup;
+        gallery.innerHTML = markup;
         lightbox.refresh();
         if (data.hits.length === 0) {
           iziToast.error({
